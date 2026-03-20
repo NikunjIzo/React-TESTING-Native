3e027c5a9363d496cbc8e7d65c1eb85237c038b9
NikunjIzo <nikunj@izonnet.com>
2026-03-20 16:53:38 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,28 +1,56 @@
 import React, { useState } from 'react';
-import { SafeAreaView, Text, Button, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, View, Button, StyleSheet } from 'react-native';
 
-export default function App() {
-  const [message, setMessage] = useState("Hello from me");
+const App = () => {
+  const [count, setCount] = useState<number>(0);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>{message}</Text>
-      <Button
-        title="Click Me"
-        onPress={() => setMessage("React Native CI/CD ")}
-      />
+      <Text style={styles.title}>🚀 My iOS CI App</Text>
+
+      <Text style={styles.subtitle}>
+        Built using GitHub Actions + macOS runner 🍏
+      </Text>
+
+      <View style={styles.counterBox}>
+        <Text style={styles.counter}>{count}</Text>
+      </View>
+
+      <Button title="Increase Count" onPress={() => setCount(count + 1)} />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#0f172a',
   },
-  text: {
-    fontSize: 24,
+  title: {
+    fontSize: 26,
+    color: '#38bdf8',
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#cbd5f5',
+    marginBottom: 30,
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+  counterBox: {
+    backgroundColor: '#1e293b',
+    padding: 20,
+    borderRadius: 10,
     marginBottom: 20,
   },
+  counter: {
+    fontSize: 32,
+    color: '#22c55e',
+  },
 });
+
+export default App;
